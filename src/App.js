8b9049b41433cac2133c6959ef094d8015262467
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-26 11:28:15 +0200

    uninstall reactstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'reactstrap';
 
 class App extends Component {
 
   render() {
     return (
-      <Button color="danger">Hello!</Button>
+      <p>test</p>
     );
   }
 }
