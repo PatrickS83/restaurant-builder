394836a886d70235c4919002f710909efa50bf7f
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-26 19:25:24 +0200

    cosmetic change by prettier-extension

--- src/components/MenuTabs.js
+++ src/components/MenuTabs.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const MenuTabs = props => {
   return (
     <div className="btn-group">
-      <button type="button" onClick={props.handleMenuClick} id="breakfast" className={`btn btn-secondary ${props.activeMenu === "breakfast" ? "active" : ""}`} >Breakfast</button>
-      <button type="button" onClick={props.handleMenuClick} id="lunch" className={`btn btn-secondary ${props.activeMenu === "lunch" ? "active" : ""}`}>Lunch</button>
-      <button type="button" onClick={props.handleMenuClick} id="dinner" className={`btn btn-secondary ${props.activeMenu === "dinner" ? "active" : ""}`}>Dinner</button>
+      <button
+        type="button"
+        onClick={props.handleMenuClick}
+        id="breakfast"
+        className={`btn btn-secondary ${props.activeMenu === "breakfast" ? "active" : ""}`}
+      >
+        Breakfast
+      </button>
+      <button
+        type="button"
+        onClick={props.handleMenuClick}
+        id="lunch"
+        className={`btn btn-secondary ${props.activeMenu === "lunch" ? "active" : ""}`}
+      >
+        Lunch
+      </button>
+      <button
+        type="button"
+        onClick={props.handleMenuClick}
+        id="dinner"
+        className={`btn btn-secondary ${props.activeMenu === "dinner" ? "active" : ""}`}
+      >
+        Dinner
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default MenuTabs;
