b2f6d535d9fe080ab2d6971f7f619b65202b53f6
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-26 11:00:41 +0200

    add bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        Hello
-      </div>
+      <Button color="danger">Hello!</Button>
     );
   }
 }
