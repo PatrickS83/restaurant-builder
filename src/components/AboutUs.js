90a90fc3143632c4ea24ce4f7777a966b33a8fb1
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-27 13:16:57 +0200

    add functionality to edit text if adminflag

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,27 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import ContentEditable from "react-contenteditable";
 
-class AboutUs extends Component {
-  render() {
-    return (
-      <section className="bg-primary" id="about">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 mx-auto text-center">
-              <h2 className="section-heading text-white">{`We've got what you need!`}</h2>
-              <hr className="light my-4" />
-              <p className="text-faded mb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ratione mollitia
-                molestias saepe est iure veniam iusto, quaerat reiciendis, ipsam quibusdam quam!
-                Iure modi neque sunt consequatur! Natus harum voluptates incidunt alias aperiam?
-                Obcaecati harum esse aspernatur rerum accusantium dignissimos dicta numquam
-                voluptatibus tempore quo ratione quisquam molestias, laudantium expedita!
-              </p>
-            </div>
+const AboutUs = props => {
+  return (
+    <section className="bg-primary" id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto text-center">
+            {props.isAdmin ? (
+              <ContentEditable
+                className="section-heading text-white"
+                html={props.data.title} // innerHTML of the editable div
+                disabled={false} // use true to disable edition
+                onChange={e => props.handleTextChange(e, "AboutUs", "title")} // handle innerHTML change
+                tagName={"h2"}
+              />
+            ) : (
+              <h2 className="section-heading text-white">{props.data.title}</h2>
+            )}
+            <hr className="light my-4" />
+            {props.isAdmin ? (
+              <ContentEditable
+                className="text-faded mb-4"
+                html={props.data.message} // innerHTML of the editable div
+                disabled={false} // use true to disable edition
+                onChange={e => props.handleTextChange(e, "AboutUs", "message")} // handle innerHTML change
+                tagName={"p"}
+              />
+            ) : (
+              <p className="text-faded mb-4">{props.data.message}</p>
+            )}
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+AboutUs.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string,
+    message: PropTypes.string
+  }),
+  isAdmin: PropTypes.bool,
+  handleTextChange: PropTypes.func
+};
 
 export default AboutUs;
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -8,12 +8,31 @@ import MenuContainer from "./MenuContainer";
 import Footer from "./Footer";
 
 class Admin extends Component {
+  state = {
+    isAdmin: true,
+    AboutUs: {
+      title: "This is a title",
+      message: "This is the messages text. This restaurant is very nice! Please buy more!"
+    }
+  };
+
+  handleTextChange = (e, compName, compProperty) => {
+    const copy = { ...this.state[compName] };
+    copy[compProperty] = e.currentTarget.innerHTML;
+    console.log(copy);
+    this.setState({ [compName]: copy });
+  };
+
   render() {
     return (
       <React.Fragment>
         <Navbar />
         <LandingPage />
-        <AboutUs />
+        <AboutUs
+          isAdmin={this.state.isAdmin}
+          data={{ ...this.state.AboutUs }}
+          handleTextChange={this.handleTextChange}
+        />
         <Specialities />
         <MenuContainer />
         <Footer />
