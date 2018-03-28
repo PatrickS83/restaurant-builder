5abf30d00ad703211b4d008e6a83a4fce9ff0c7f
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-28 17:46:54 +0200

    tempory route change to always admin

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import React from 'react';
-import Admin from './Admin';
-import App from '../App';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import Admin from "./Admin";
+import App from "../App";
 // import NotFound from './NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/admin' component={Admin} />
+      <Route exact path="/" component={Admin} />
+      <Route path="/admin" component={Admin} />
     </Switch>
   </BrowserRouter>
 );
