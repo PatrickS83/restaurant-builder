2cbd731c8f2fd075efe3e506e432734df7ae3f75
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-28 12:50:13 +0200

    temporarily disable serviceworker for deployment test

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import Router from './components/Router';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
