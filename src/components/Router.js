04759e5981a7880a884642dafaac9fe3ed7475ce
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-28 13:09:21 +0200

    trying deployment without hashHistory

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React from "react";
-import Admin from "./Admin";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import Admin from './Admin';
 // import App from "../App";
 // import NotFound from './NotFound';
-import createHashHistory from 'history/createHashHistory';
+import createHistory from 'history/createBrowserHistory';
 
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+const history = createHistory({ basename: process.env.PUBLIC_URL });
 
 const Router = () => (
-  <BrowserRouter history={hashHistory}>
+  <BrowserRouter history={history}>
     <Switch>
       <Route exact path="/" component={Admin} />
       <Route path="/admin" component={Admin} />
