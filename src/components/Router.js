e2e5464298b340cf714f08e9d6202f2a51ace5c1
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-28 12:42:27 +0200

    made router compatible with github pages

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,9 +3,12 @@ import React from "react";
 import Admin from "./Admin";
 // import App from "../App";
 // import NotFound from './NotFound';
+import createHashHistory from 'history/createHashHistory';
+
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 const Router = () => (
-  <BrowserRouter>
+  <BrowserRouter history={hashHistory}>
     <Switch>
       <Route exact path="/" component={Admin} />
       <Route path="/admin" component={Admin} />
